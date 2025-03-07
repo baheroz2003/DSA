a28c783dce13c3504ebefebe51c2464e64e774e0
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-03-07 17:39:19 +0530

    Update backtracking.cpp

--- backtracking.cpp
+++ backtracking.cpp
@@ -1,3 +1,4 @@
+#type 1:order matters but move to next index
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -30,3 +31,42 @@ class Solution {
             return ans.size()-1;
         }
     };
+#type 2:
+//move to next index  order doesn't matters
+void subsetHelper(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& result) {
+    result.push_back(ds);
+    for (int i = index; i < nums.size(); i++) {
+        ds.push_back(nums[i]);
+        subsetHelper(i + 1, nums, ds, result);
+        ds.pop_back();
+    }
+}
+vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> ds;
+    subsetHelper(0, nums, ds, result);
+    return result;
+}
+#type 3:
+//order doesn't matters but stay at same index
+void combinationHelper(int index, vector<int>& nums, int target, vector<int>& ds, vector<vector<int>>& result) {
+    if (target == 0) {
+        result.push_back(ds);
+        return;
+    }
+
+    for (int i = index; i < nums.size(); i++) {
+        if (nums[i] > target) continue;
+        ds.push_back(nums[i]);
+        combinationHelper(i, nums, target - nums[i], ds, result);
+        ds.pop_back();
+    }
+}
+
+vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<vector<int>> result;
+    vector<int> ds;
+    combinationHelper(0, nums, target, ds, result);
+    return result;
+}
+
