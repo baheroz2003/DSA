80dff6b17214df834a0ad154659d219df4050678
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-03-07 09:41:43 +0530

    Update sieve.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -21,41 +21,18 @@ int main(){
     }
 }
 
-//eriuuhvr
-
-/*#include <bits/stdc++.h>
+#modified sieve optimised
+#include <bits./stdc++.h>
 using namespace std;
-
-void solve(){
-    int n;
-    cin >> n;
-    n++;
-    
-    vector<bool>store(n,true);
-    store[0] = store[1] = false;
-
-    for(int i=0; i<n; i++){
-        if(store[i]){
-            for(int j = i*2; j<n; j += i){
-                store[j] = false;
+vector<int> store;  
+void seive(int n) {
+    store.assign(n + 1, 1);
+    store[0] = store[1] = 0;
+    for(int i = 2; i * i <= n; i++) {
+        if(store[i]) {
+            for(int j = i * i; j <= n; j += i) {
+                store[j] = 0;
             }
         }
     }
-    
-    int sum=0;
-
-    for(int i=0; i<n; i++){
-        if(store[i]) sum += i;
-    }
-    cout << sum << endl;
 }
-
-int main(){
-    int t;
-    cin >> t;
-    
-    while(t--){
-        solve();
-    }
-    return 0;
-}*/
