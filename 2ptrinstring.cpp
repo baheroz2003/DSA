c002e6845f861cca96ad117e41c53a8031f4f1a2
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-09-20 23:22:10 +0530

    Delete 2ptrinstring.cpp

--- 2ptrinstring.cpp
+++
@@ -1,3 +0,0 @@
-Jab bhi tumhe string ke andar parts read karne hote hain jahan ek delimiter (like #, space, comma) hota hai, ya tumhe number+string format parse karna hota hai → two pointer ka use hota hai:
-Pehla pointer = starting point
-Dusra pointer = search for delimiter/end
