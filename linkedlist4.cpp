62f04fcb5fce623ebcff779f76bdd0e39e975062
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-09-21 11:07:33 +0530

    Update linkedlist4.cpp

--- linkedlist4.cpp
+++ linkedlist4.cpp
@@ -29,3 +29,17 @@ public:
         return newHead;
     }
 };
+#middle of the linked list
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+       return slow; 
+    }
+};
