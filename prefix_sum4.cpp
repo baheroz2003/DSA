cd490e433e16d18ddee8c9ae300537e6dbe18d25
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-10-14 14:01:15 +0530

    Update prefix_sum4.cpp

--- prefix_sum4.cpp
+++ prefix_sum4.cpp
@@ -6,3 +6,6 @@
 //ab prefix sum 0 2 2 0 0 0
 //hashing bhi hai
 //l-r replace a[l]+=k,a[r+1]-=k find prefix sum
+//sidhi si paath hm aise krke prefix lenge toh required sum aajayega jo chahiye
+//kya kro jaise tmhe chahiye ki itne se itne range me chahiye 
+//start index +val krdo aur fir end+1 index pe -val krdo aur phir prefux sum nikaalo
