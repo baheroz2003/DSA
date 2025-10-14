3c313023bf0d04b73e57a1fa775d34ed344a6ef5
Baheroz <141439610+baheroz2003@users.noreply.github.com>
2025-10-14 14:24:06 +0530

    Update prefix_sum4.cpp
    added

--- prefix_sum4.cpp
+++ prefix_sum4.cpp
@@ -9,3 +9,19 @@
 //sidhi si paath hm aise krke prefix lenge toh required sum aajayega jo chahiye
 //kya kro jaise tmhe chahiye ki itne se itne range me chahiye 
 //start index +val krdo aur fir end+1 index pe -val krdo aur phir prefux sum nikaalo
+long arrayManipulation(int n, vector<vector<int>>queries) {
+       vector<long long>arr(n+2,0);
+       for(auto x:queries){
+        int st=x[0];
+        int end=x[1];
+        long long val=x[2];
+        arr[st]+=val;
+        arr[end+1]-=val;
+       }
+       for(int i=1;i<=n;i++){
+        arr[i]=arr[i-1]+arr[i];
+       }
+       long long ans=*max_element(arr.begin(),arr.end());
+       return ans;
+       }
+
